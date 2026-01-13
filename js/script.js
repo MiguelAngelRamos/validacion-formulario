d728a43886e3c59c68b675818d4513c230f31b5b
Miguel Ramos <return.infox@gmail.com>
2026-01-13 20:20:30 -0300

    formulario de validacion terminado

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const VALIDATION_RULES = {
     // Valida: usuario @ dominio . extension (min 2 letras)
     email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
     // Valida: Mínimo 8 caracteres, al menos una letra y un número
+    // secreto123
     password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 };
+
 
 const loginForm = document.getElementById('login-form');
 // const loginForm = document.querySelector('#login-form');
@@ -13,8 +15,21 @@ const emailInput = document.querySelector('[data-js="email-input"]');
 const passInput = document.querySelector('[data-js="password-input"]');
 
 // los spans de errores
-const emailError = document.querySelector('[data-js="email-error"]');
-const passError = document.querySelector('[data-js="password-error"]');
+const emailErrorSpan = document.querySelector('[data-js="email-error"]');
+const passErrorSpan = document.querySelector('[data-js="password-error"]');
+
+// Funcion para mostrar u ocultar errores (Reutilizable)
+
+const toggleError = (input, errorElementSpan, message, isInvalid) => {
+  // errorElementSpan.textContent nos permite escribir dentro del span
+  // si isInvalid es true mostramos el mensaje, si no lo es, mostramos una cadena vacia
+  // si es true isInvalid entonces el span queda asi <span class="error-msg" data-js="email-error">Por favor ingresa un correo válido</span>
+  // si es false isInvalid entonces el span queda asi <span class="error-msg" data-js="email-error"></span>
+  errorElementSpan.textContent = isInvalid ? message: '';
+  isInvalid ? input.classList.add('invalid') : input.classList.remove('invalid');
+}
+
+
 
 loginForm.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -23,15 +38,25 @@ loginForm.addEventListener('submit', function(event) {
   const emailValue = emailInput.value.trim();
   const passValue = passInput.value.trim();
  
-  // validar email sofia@correo.com
+  //* StartEmail
+  // validar email sofia@correo.com  si pasamos a VALIDATION_RULES.email.test(emailValue); nos retorna true
   const isEmailInvalid = !VALIDATION_RULES.email.test(emailValue);
-  console.log(isEmailInvalid);
-  //* Si cumple con el formato de correo retorna true
 
+  // emailInput es <input type="email" id="email" data-js="email-input" placeholder="ejemplo@correo.com">
+  // emailErrorSpan es <span class="error-msg" data-js="email-error"></span>
+  // isEmailInvalid es true o false dependiendo del resultado de la expresion regular y la negacion final, recordar que cuando sea totalmente valido VALIDATION_RULES.email.test(emailValue); retorna true, pero al negarlo con ! se vuelve false
+
+
+  toggleError(emailInput, emailErrorSpan, 'Por favor ingresa un correo válido', isEmailInvalid);
+  //* EndEmail
+
+  //* StartPassword
   // validar password Academy123
   const isPassInvalid = !VALIDATION_RULES.password.test(passValue);
   console.log(isPassInvalid);
-  //* Si cumple con el formato  de password retorna true
+  toggleError(passInput, passErrorSpan, 'Por favor ingresa una contraseña válida', isPassInvalid);
+  //* End Password
+
 
   // * Esta condición va ser verdadera siempre y cuando el email y el password sean validos
 
